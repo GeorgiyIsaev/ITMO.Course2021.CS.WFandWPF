42575916285946a989b350cd7c0919390892f0d9
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-15 21:20:44 +0300

    add less01 ex06

---
+++ WF.Lessons/Lesson01/WF.Lesson01.Ex06.WinFormsAppScr/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsAppScr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width -
+                this.Width, Screen.PrimaryScreen.Bounds.Height - this.Height);
+
+         //   this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width 
+         //       - Width, Screen.PrimaryScreen.WorkingArea.Height - Height);
+
+          //  Size s = SystemInformation.PrimaryMonitorSize;
+         //   this.Location = new Point(s.Width - this.Width, s.Height - s.Height); // это верхний правый угол
+        }
+    }
+}
