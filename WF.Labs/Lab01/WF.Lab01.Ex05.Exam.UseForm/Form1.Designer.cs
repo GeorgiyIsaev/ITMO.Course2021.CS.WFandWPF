29704b2e8ab677e3effea762f9cd1eb000ac466d
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-16 22:55:48 +0300

    Lab01 ex05 task01

--- WF.Labs/Lab01/WF.Lab01.Ex05.Exam.UseForm/Form1.Designer.cs
+++ WF.Labs/Lab01/WF.Lab01.Ex05.Exam.UseForm/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Button_Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Button_Exit
+            // 
+            this.Button_Exit.Location = new System.Drawing.Point(291, 197);
+            this.Button_Exit.Name = "Button_Exit";
+            this.Button_Exit.Size = new System.Drawing.Size(75, 23);
+            this.Button_Exit.TabIndex = 0;
+            this.Button_Exit.Text = "Закрыть";
+            this.Button_Exit.UseVisualStyleBackColor = true;
+            this.Button_Exit.Click += new System.EventHandler(this.Button_Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Button_Exit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Button_Exit;
     }
 }
 
--- WF.Labs/Lab01/WF.Lab01.Ex05.Exam.UseForm/Form1.cs
+++ WF.Labs/Lab01/WF.Lab01.Ex05.Exam.UseForm/Form1.cs
@@ -16,5 +16,19 @@ namespace WF.Lab01.Ex05.Exam.UseForm
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath =
+                new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddEllipse(0, 0, this.Width, this.Height);
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+        }
+
+        private void Button_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
