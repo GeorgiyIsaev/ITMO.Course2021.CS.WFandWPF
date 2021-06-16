cfeb13edd98180ddf1938f4c9ed5b23f5c41bc5e
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-16 19:53:04 +0300

    add less02 ex02

---
+++ WF.Lessons/Lesson02/WF.Lesson02.Ex02.WinFormsSingleton/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsSingleton
+{
+    public partial class Form1 : Form
+    {
+        Form2 mf;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          mf = Form2.metf2;
+ 
+              mf.Show();
+              mf.Activate();
+        }
+    }
+}
---
+++ WF.Lessons/Lesson02/WF.Lesson02.Ex02.WinFormsSingleton/Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsSingleton
+{
+    public partial class Form2 : Form
+    {
+        static Form2 mF2 = null;
+
+        Form2() // конструктор закрыт
+        {
+            InitializeComponent();
+        }
+
+        /*
+         объект данного класса возможно получить вызвав открытое статическое свойство MyInstance, которое всегда возвращает существующий объект, 
+         * или предварительно создает его в случае первого обращения
+         */
+        public static Form2 metf2
+        {
+            get
+            {
+                if (mF2 == null)
+                {
+                    mF2 = new Form2();
+                }
+                return mF2;
+            }
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            mF2 = null;
+        }
+    }
+}
