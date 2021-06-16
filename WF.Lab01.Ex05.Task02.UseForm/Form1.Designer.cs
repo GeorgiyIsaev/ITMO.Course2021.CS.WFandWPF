dffb4003a1aa305decd9db752589757d3d477613
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-16 23:32:46 +0300

    full lab01 ex05 task2 (add форма ромба)

--- WF.Lab01.Ex05.Task02.UseForm/Form1.Designer.cs
+++ WF.Lab01.Ex05.Task02.UseForm/Form1.Designer.cs
@@ -28,21 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Button_Hide = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // Button_Hide
+            // 
+            this.Button_Hide.Location = new System.Drawing.Point(221, 113);
+            this.Button_Hide.Name = "Button_Hide";
+            this.Button_Hide.Size = new System.Drawing.Size(75, 23);
+            this.Button_Hide.TabIndex = 0;
+            this.Button_Hide.Text = "Скрыть";
+            this.Button_Hide.UseVisualStyleBackColor = true;
+            this.Button_Hide.Click += new System.EventHandler(this.Button_Hide_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Lime;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.Green;
+            this.textBox1.Location = new System.Drawing.Point(167, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(186, 35);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "GREENPEACE";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(521, 250);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Button_Hide);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Button_Hide;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WF.Lab01.Ex05.Task02.UseForm/Form1.cs
+++ WF.Lab01.Ex05.Task02.UseForm/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,26 @@ namespace WF.Lab01.Ex05.Task02.UseForm
         {
             e.Cancel = true;
             Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (GraphicsPath myPath = new GraphicsPath())
+            {
+                myPath.AddLines(new[]
+                {
+                    new Point(0, Height / 2),
+                    new Point(Width / 2, 0),
+                    new Point(Width, Height / 2),
+                    new Point(Width / 2, Height)
+                });
+                Region = new Region(myPath);
+            }
+        }
+
+        private void Button_Hide_Click(object sender, EventArgs e)
+        {
+            this.Hide();
         }
     }
 }
