f70c4e0cb868b301f0370392aa694dcf86172f72
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-16 21:00:23 +0300

    add less02 ex08

---
+++ WF.Lessons/Lesson02/WF.Lesson02.Ex08.Sample_Delegate/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Sample_Delegate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.percentRadioButton = new System.Windows.Forms.RadioButton();
+            this.dollarRadioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(21, 31);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(100, 20);
+            this.TextBox1.TabIndex = 0;
+            this.TextBox1.Text = "1,25";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Format Text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox2.Location = new System.Drawing.Point(285, 30);
+            this.TextBox2.Multiline = true;
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(100, 23);
+            this.TextBox2.TabIndex = 2;
+            // 
+            // percentRadioButton
+            // 
+            this.percentRadioButton.AutoSize = true;
+            this.percentRadioButton.Location = new System.Drawing.Point(21, 82);
+            this.percentRadioButton.Name = "percentRadioButton";
+            this.percentRadioButton.Size = new System.Drawing.Size(129, 17);
+            this.percentRadioButton.TabIndex = 3;
+            this.percentRadioButton.TabStop = true;
+            this.percentRadioButton.Text = "Format as Percentage";
+            this.percentRadioButton.UseVisualStyleBackColor = true;
+            this.percentRadioButton.CheckedChanged += new System.EventHandler(this.percentRadioButton_CheckedChanged);
+            // 
+            // dollarRadioButton
+            // 
+            this.dollarRadioButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.dollarRadioButton.AutoSize = true;
+            this.dollarRadioButton.Location = new System.Drawing.Point(285, 82);
+            this.dollarRadioButton.Name = "dollarRadioButton";
+            this.dollarRadioButton.Size = new System.Drawing.Size(101, 17);
+            this.dollarRadioButton.TabIndex = 4;
+            this.dollarRadioButton.TabStop = true;
+            this.dollarRadioButton.Text = "Format as Dollar";
+            this.dollarRadioButton.UseVisualStyleBackColor = true;
+            this.dollarRadioButton.CheckedChanged += new System.EventHandler(this.dollarRadioButton_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(21, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 50);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button6, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(227, 114);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(159, 97);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(82, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 51);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(73, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(82, 51);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(73, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(413, 226);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dollarRadioButton);
+            this.Controls.Add(this.percentRadioButton);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TextBox2;
+        private System.Windows.Forms.RadioButton percentRadioButton;
+        private System.Windows.Forms.RadioButton dollarRadioButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WF.Lessons/Lesson02/WF.Lesson02.Ex08.Sample_Delegate/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sample_Delegate
+{
+    public partial class Form1 : Form
+    {
+        FormatNumbers.FormatSingle formatText;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.TextBox2.Text = formatText(Convert.ToSingle(TextBox1.Text));
+        }
+
+        private void percentRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.percentRadioButton.Checked)
+            {
+                formatText = new FormatNumbers.FormatSingle(FormatNumbers.returnPercentage);
+            }
+        }
+
+        private void dollarRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.dollarRadioButton.Checked)
+            {
+                formatText = new FormatNumbers.FormatSingle(FormatNumbers.returnDollars);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            formatText = new FormatNumbers.FormatSingle(FormatNumbers.returnPercentage);
+        }
+    }
+    public class FormatNumbers
+    {
+        public delegate string FormatSingle(float number);
+
+        public static string returnDollars(float number)
+        {
+            return string.Format("{0:C}", number);
+        }
+
+        public static string returnPercentage(float number)
+        {
+            return string.Format("{0:P}", number);
+        }
+    }
+}
