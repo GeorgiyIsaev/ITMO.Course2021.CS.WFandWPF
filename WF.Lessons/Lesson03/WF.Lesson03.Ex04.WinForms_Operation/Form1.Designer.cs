8ce824faea94fa31275c59ebbd8bafff00920cdd
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-17 19:46:36 +0300

    add less03 ex04

---
+++ WF.Lessons/Lesson03/WF.Lesson03.Ex04.WinForms_Operation/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WinForms_Operation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxF1 = new System.Windows.Forms.TextBox();
+            this.buttonF1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxF1
+            // 
+            this.textBoxF1.Location = new System.Drawing.Point(12, 12);
+            this.textBoxF1.Name = "textBoxF1";
+            this.textBoxF1.ReadOnly = true;
+            this.textBoxF1.Size = new System.Drawing.Size(373, 20);
+            this.textBoxF1.TabIndex = 0;
+            // 
+            // buttonF1
+            // 
+            this.buttonF1.Location = new System.Drawing.Point(17, 38);
+            this.buttonF1.Name = "buttonF1";
+            this.buttonF1.Size = new System.Drawing.Size(368, 23);
+            this.buttonF1.TabIndex = 1;
+            this.buttonF1.Text = "Открыть вторую форму для ввода данных";
+            this.buttonF1.UseVisualStyleBackColor = true;
+            this.buttonF1.Click += new System.EventHandler(this.buttonF1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 79);
+            this.Controls.Add(this.buttonF1);
+            this.Controls.Add(this.textBoxF1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxF1;
+        private System.Windows.Forms.Button buttonF1;
+    }
+}
+
