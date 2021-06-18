6cb6befc38aadbaa7653ea48feab3cd29ea3d367
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-18 22:48:47 +0300

    full lab02 ex02

--- WF.Labs/Lab02/WF.Lab02.Ex02.ListBox/TestList.Designer.cs
+++ WF.Labs/Lab02/WF.Lab02.Ex02.ListBox/TestList.Designer.cs
@@ -34,6 +34,7 @@
             this.buttonAdd = new System.Windows.Forms.Button();
             this.buttonDelete = new System.Windows.Forms.Button();
             this.buttonSort = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -64,14 +65,14 @@
             "Данил",
             "Петр",
             "Майкл"});
-            this.peopleList.Location = new System.Drawing.Point(234, 24);
+            this.peopleList.Location = new System.Drawing.Point(230, 73);
             this.peopleList.Name = "peopleList";
             this.peopleList.Size = new System.Drawing.Size(121, 21);
             this.peopleList.TabIndex = 1;
             // 
             // buttonAdd
             // 
-            this.buttonAdd.Location = new System.Drawing.Point(253, 69);
+            this.buttonAdd.Location = new System.Drawing.Point(253, 100);
             this.buttonAdd.Name = "buttonAdd";
             this.buttonAdd.Size = new System.Drawing.Size(75, 23);
             this.buttonAdd.TabIndex = 2;
@@ -99,11 +100,22 @@
             this.buttonSort.UseVisualStyleBackColor = true;
             this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
             // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(230, 24);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(121, 29);
+            this.buttonLoad.TabIndex = 5;
+            this.buttonLoad.Text = "Загрузить данные";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
             // TestList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(380, 258);
+            this.Controls.Add(this.buttonLoad);
             this.Controls.Add(this.buttonSort);
             this.Controls.Add(this.buttonDelete);
             this.Controls.Add(this.buttonAdd);
@@ -125,6 +137,7 @@
         private System.Windows.Forms.Button buttonAdd;
         private System.Windows.Forms.Button buttonDelete;
         private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Button buttonLoad;
     }
 }
 
--- WF.Labs/Lab02/WF.Lab02.Ex02.ListBox/TestList.cs
+++ WF.Labs/Lab02/WF.Lab02.Ex02.ListBox/TestList.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace WF.Lab02.Ex02.ListBox
 {
@@ -35,6 +37,30 @@ namespace WF.Lab02.Ex02.ListBox
         private void buttonSort_Click(object sender, EventArgs e)
         {
             memberList.Sorted = true;
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            peopleList.Items.Clear();
+            try
+            {
+                using (FileStream fStream =
+                    new FileStream("..\\..\\XMLFile1.xml",
+                    FileMode.Open,
+                    FileAccess.Read,
+                    FileShare.ReadWrite))
+                {
+                    XmlDocument xmlDoc = new XmlDocument(); xmlDoc.Load(fStream);
+                    for (int i = 0; i < xmlDoc.DocumentElement.ChildNodes.Count; i++)
+                    {
+                        peopleList.Items.Add(xmlDoc.DocumentElement.ChildNodes[i].InnerText);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR! Нет доступа к файлу XMLFile1.xml\n" + ex.Message);
+            }
         }
     }
 }
