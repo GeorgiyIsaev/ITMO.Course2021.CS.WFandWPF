ea50d01bde6e8048c249038b3a0c70fa8f8ec77f
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-18 22:25:54 +0300

    full lab02 ex01

--- WF.Lab02.Ex01.EventClick/Program.cs
+++ WF.Lab02.Ex01.EventClick/Program.cs
@@ -16,7 +16,7 @@ namespace WF.Lab02.Ex01.EventClick
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new WinQuestion());
         }
     }
 }
