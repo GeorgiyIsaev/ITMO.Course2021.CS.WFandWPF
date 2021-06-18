ca81b20aa12d1bbaa48e8a09969a80567961dc1d
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-18 22:40:18 +0300

    lab02 ex02 p10

--- WF.Labs/Lab02/WF.Lab02.Ex02.ListBox/Program.cs
+++ WF.Labs/Lab02/WF.Lab02.Ex02.ListBox/Program.cs
@@ -16,7 +16,7 @@ namespace WF.Lab02.Ex02.ListBox
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new TestList());
         }
     }
 }
