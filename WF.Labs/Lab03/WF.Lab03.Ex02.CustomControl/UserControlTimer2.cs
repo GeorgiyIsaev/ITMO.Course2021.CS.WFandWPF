c33ead8ee443c03e37367096019813810d903da4
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-19 23:29:46 +0300

    create lab03 ex02

---
+++ WF.Labs/Lab03/WF.Lab03.Ex02.CustomControl/UserControlTimer2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF.Lab03.Ex02.CustomControl
+{
+    public partial class UserControlTimer2 : Control
+    {
+        public UserControlTimer2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+    }
+}
