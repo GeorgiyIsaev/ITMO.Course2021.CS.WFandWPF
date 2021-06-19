cad7e225ffd0752617bcc079b2a4bfcbe7fcddfd
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-19 23:44:41 +0300

    WF.Lab03.Ex04

---
+++ WF.Labs/Lab03/WF.Lab03.Ex04.Task01.LabelValid/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF.Lab03.Ex04.Task01.LabelValid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WF.Labs/Lab03/WF.Lab03.Ex04.Task01.LabelValid/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF.Lab03.Ex04.Task01.LabelValid
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
