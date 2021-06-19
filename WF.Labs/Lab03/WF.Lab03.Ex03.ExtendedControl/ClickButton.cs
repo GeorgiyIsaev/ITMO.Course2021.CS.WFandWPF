937b726ced647da4be4bacc3b4e168a3c99eb7e4
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-19 23:40:09 +0300

    create lab03 ex03

---
+++ WF.Labs/Lab03/WF.Lab03.Ex03.ExtendedControl/ClickButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF.Lab03.Ex03.ExtendedControl
+{
+    class ClickButton : System.Windows.Forms.Button
+    {
+        int mClicks;
+        public int Clicks
+        {
+            get { return mClicks; }
+
+        }
+    }
+}
