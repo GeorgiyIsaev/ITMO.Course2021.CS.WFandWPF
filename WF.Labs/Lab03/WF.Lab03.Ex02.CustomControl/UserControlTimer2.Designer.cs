f4d3ab9bd1a03c72a3c67dd72e0f93b9375f74ea
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-19 23:36:03 +0300

    lab03 ex02 p9

--- WF.Labs/Lab03/WF.Lab03.Ex02.CustomControl/UserControlTimer2.Designer.cs
+++ WF.Labs/Lab03/WF.Lab03.Ex02.CustomControl/UserControlTimer2.Designer.cs
@@ -28,9 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+
     }
 }
--- WF.Labs/Lab03/WF.Lab03.Ex02.CustomControl/UserControlTimer2.cs
+++ WF.Labs/Lab03/WF.Lab03.Ex02.CustomControl/UserControlTimer2.cs
@@ -20,6 +20,16 @@ namespace WF.Lab03.Ex02.CustomControl
         protected override void OnPaint(PaintEventArgs pe)
         {
             base.OnPaint(pe);
+            Graphics g = pe.Graphics;
+            g.FillRectangle(Brushes.Blue, 0, 0, this.Width, this.Height);
+            pe.Graphics.DrawString(DateTime.Now.ToLongTimeString(), this.Font, new SolidBrush(this.ForeColor), 0, 0);
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+      
     }
 }
