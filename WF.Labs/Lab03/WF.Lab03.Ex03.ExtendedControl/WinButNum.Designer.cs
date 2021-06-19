442fbf0ee6905199c72f5a326270791596b0bd79
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-19 23:41:35 +0300

    full lab03 ex03

--- WF.Labs/Lab03/WF.Lab03.Ex03.ExtendedControl/WinButNum.Designer.cs
+++ WF.Labs/Lab03/WF.Lab03.Ex03.ExtendedControl/WinButNum.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.clickButton1 = new WF.Lab03.Ex03.ExtendedControl.ClickButton();
+            this.SuspendLayout();
+            // 
+            // clickButton1
+            // 
+            this.clickButton1.Location = new System.Drawing.Point(28, 24);
+            this.clickButton1.Name = "clickButton1";
+            this.clickButton1.Size = new System.Drawing.Size(145, 23);
+            this.clickButton1.TabIndex = 0;
+            this.clickButton1.Text = "clickButton1";
+            this.clickButton1.UseVisualStyleBackColor = true;
+            // 
+            // WinButNum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clickButton1);
+            this.Name = "WinButNum";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ClickButton clickButton1;
     }
 }
 
