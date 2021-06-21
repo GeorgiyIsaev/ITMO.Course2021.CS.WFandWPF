75db83198abb9bebdf8bb1b04a4762becaaeb8ab
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-21 20:48:43 +0300

    add less04 ex13, ex15

---
+++ WF.Lessons/Lesson04/WF.Lesson04.Ex14.Factorialasync/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Factorialasync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++) 
+                DisplayResultAsync();
+
+
+
+            for (int i = 1; i <= 10; i++)
+            { Thread.Sleep(1000); Console.WriteLine("Печать ..."); }
+            Console.ReadLine();
+
+        }
+
+        static async void  DisplayResultAsync()
+        {
+            int num = 5;
+
+            int result = await FactorialAsync(num);
+            Thread.Sleep(3000);
+            Console.WriteLine("Факториал числа {0} равен {1}", num, result);
+        }
+
+        static Task<int> FactorialAsync(int x)
+        {
+            int result = 1;
+
+            return Task.Run(() =>
+            {
+                for (int i = 1; i <= x; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            });
+        }
+
+    }
+}
---
+++ WF.Lessons/Lesson04/WF.Lesson04.Ex15.FactorialAsyncWait/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FactorialAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayResultAsync();
+
+            //Task t = DisplayResultAsync();
+            //t.Wait();
+
+            Console.ReadLine();
+        }
+
+        static async void DisplayResultAsync()
+        {
+            int num = 5;
+
+            int result = await FactorialAsync(num);
+            Thread.Sleep(3000);
+            Console.WriteLine("Факториал числа {0} равен {1}", num, result);
+        }
+
+        //static async Task DisplayResultAsync()
+        //{
+        //    int num = 5;
+
+        //    int result = await FactorialAsync(num);
+        //    Thread.Sleep(3000);
+        //    Console.WriteLine("Факториал числа {0} равен {1}", num, result);
+        //}
+
+        static Task<int> FactorialAsync(int x)
+        {
+            int result = 1;
+
+            return Task.Run(() =>
+            {
+                for (int i = 1; i <= x; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            });
+        }
+
+
+
+
+    }
+}
