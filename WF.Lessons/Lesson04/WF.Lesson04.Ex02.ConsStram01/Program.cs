2c29a00758519c4dd13f88b1ba889df43d069f27
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-21 19:01:29 +0300

    add less04 ex02, ex03

---
+++ WF.Lessons/Lesson04/WF.Lesson04.Ex02.ConsStram01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void SimpleWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(1000);
+            }
+               
+        }
+        
+        static void Main(string[] args)
+        {
+          // Создаем новый поток 
+            Thread theThread = new Thread(new ThreadStart(SimpleWork));
+            theThread.IsBackground = true;
+            // Запускаем задачу в новом потоке 
+                theThread.Start();
+            // моделирование работы основного потока
+             string s;
+            do
+            {
+                s = Console.ReadLine();
+                Console.WriteLine(s);
+            } while (s != "q");            
+        }
+     }
+}
+
+
---
+++ WF.Lessons/Lesson04/WF.Lesson04.Ex03.ConsStram02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void SimpleWork()
+        {
+            for (int x = 1; x <= 10; ++x) {
+            Console.WriteLine("Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+                // Замедляем работу потока, позволяя другим потокам продолжить выполнение
+                Thread.Sleep(100);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadStart operation = new ThreadStart(SimpleWork);
+            for (int x = 1; x <= 5; ++x)
+            {
+                // Создаем новый поток, но не запускаем его
+                Thread theThread = new Thread(operation);
+                // Запускаем задачу в новом потоке 
+                theThread.Start();
+            }
+
+        }
+    }
+}
