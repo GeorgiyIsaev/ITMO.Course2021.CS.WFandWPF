8553bccf6451aeaef8e24867bc03a41827a1a8e1
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-21 19:27:44 +0300

    add less04 ex05, ex06

---
+++ WF.Lessons/Lesson04/WF.Lesson04.Ex05.Task01/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  Task task = new Task(Work);
+            Task task = new Task(new Action(Work));
+            task.Start();
+
+            Task task1 = new Task(() => Work()); // Using a Lambda Expression to Invoke a Named Method
+
+            task1.Start();
+
+            Task task2 = new Task(delegate { Console.WriteLine("The time now is {0}", DateTime.Now); });
+            task2.Start();
+
+
+            Task task3 = new Task(() => { Console.WriteLine("The time now is {0}", DateTime.Now); }); //Using a Lambda Expression to Invoke an Anonymous Method
+            task3.Start();
+
+            Task task4 = Task.Run(() => { Console.WriteLine("Day of the year {0}", DateTime.Now.DayOfYear); });
+
+            Console.WriteLine("Выполняется работа метода Main");
+            string s;
+            do
+            {
+                s = Console.ReadLine();
+                Console.WriteLine(s);
+            } while (s != "q");
+
+            task.Wait();
+
+        }
+
+        static void Work()
+        {
+            Console.WriteLine("Начало работы метода Work");
+            // имитация работы метода
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Завершение работы метода Work");
+        }
+
+    }
+}
+
---
+++ WF.Lessons/Lesson04/WF.Lesson04.Ex06.Task02/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Task<string> task = Task.Run<string>(() => DateTime.Now.DayOfWeek.ToString());
+            
+            Console.WriteLine(task.Result);
+
+            Task<int> task1 = new Task<int>(() => Factorial(5));
+            task1.Start();
+
+            Console.WriteLine("Факториал числа 5 равен {0}", task1.Result);
+
+            Task<Book> task2 = new Task<Book>(() =>
+            {
+                return new Book { Title = "Book", Author = "Autor" };
+            });
+            task2.Start();
+
+            Book b = task2.Result;
+            Console.WriteLine("Название книги: {0}, автор: {1}", b.Title, b.Author);
+
+            Console.ReadLine();
+        }
+
+
+        static int Factorial(int x)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= x; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+
+
+}
