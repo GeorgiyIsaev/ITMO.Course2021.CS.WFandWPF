db85d4c806f59e60dc1a47029c4d79007b870b5d
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-22 01:20:53 +0300

    cope ex02 to ex03

---
+++ WF.Labs/Lab04/WF.Lab04.Ex03.InteractionOfComponentsAndClass/EditForms.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WF.Lab04.Ex03.InteractionOfComponentsAndClass
+{
+    partial class EditForms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("Имя");
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("Фамилия");
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("Возраст");
+            this.personsListView = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // personsListView
+            // 
+            this.personsListView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.personsListView.HideSelection = false;
+            this.personsListView.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem4,
+            listViewItem5,
+            listViewItem6});
+            this.personsListView.Location = new System.Drawing.Point(0, 0);
+            this.personsListView.Name = "personsListView";
+            this.personsListView.Size = new System.Drawing.Size(491, 172);
+            this.personsListView.TabIndex = 0;
+            this.personsListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(312, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Редактировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // EditForms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 222);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.personsListView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "EditForms";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView personsListView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ WF.Labs/Lab04/WF.Lab04.Ex03.InteractionOfComponentsAndClass/EditPersonForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WF.Lab04.Ex03.InteractionOfComponentsAndClass
+{
+    partial class EditPersonForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ageNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.Button_Save = new System.Windows.Forms.Button();
+            this.Button_Cansel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ageNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(113, 21);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(205, 20);
+            this.firstNameTextBox.TabIndex = 1;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(113, 47);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(205, 20);
+            this.lastNameTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Возраст";
+            // 
+            // ageNumericUpDown
+            // 
+            this.ageNumericUpDown.Location = new System.Drawing.Point(113, 74);
+            this.ageNumericUpDown.Name = "ageNumericUpDown";
+            this.ageNumericUpDown.Size = new System.Drawing.Size(205, 20);
+            this.ageNumericUpDown.TabIndex = 6;
+            // 
+            // Button_Save
+            // 
+            this.Button_Save.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.Button_Save.Location = new System.Drawing.Point(37, 113);
+            this.Button_Save.Name = "Button_Save";
+            this.Button_Save.Size = new System.Drawing.Size(75, 23);
+            this.Button_Save.TabIndex = 7;
+            this.Button_Save.Text = "Сохранить";
+            this.Button_Save.UseVisualStyleBackColor = true;
+            // 
+            // Button_Cansel
+            // 
+            this.Button_Cansel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Button_Cansel.Location = new System.Drawing.Point(243, 113);
+            this.Button_Cansel.Name = "Button_Cansel";
+            this.Button_Cansel.Size = new System.Drawing.Size(75, 23);
+            this.Button_Cansel.TabIndex = 8;
+            this.Button_Cansel.Text = "Отмена";
+            this.Button_Cansel.UseVisualStyleBackColor = true;
+            // 
+            // EditPersonForm
+            // 
+            this.AcceptButton = this.Button_Save;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 161);
+            this.Controls.Add(this.Button_Cansel);
+            this.Controls.Add(this.Button_Save);
+            this.Controls.Add(this.ageNumericUpDown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "EditPersonForm";
+            this.ShowInTaskbar = false;
+            this.Text = "Редактирование сотрудника";
+            ((System.ComponentModel.ISupportInitialize)(this.ageNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown ageNumericUpDown;
+        private System.Windows.Forms.Button Button_Save;
+        private System.Windows.Forms.Button Button_Cansel;
+    }
+}
