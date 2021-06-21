de2263fa7690478b1b374807682c713f9103cf65
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-21 21:38:56 +0300

    add less04 ex18

---
+++ WF.Lessons/Lesson04/WF.Lesson04.Ex18.FibonacciForm/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace FibonacciForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.startAsyncButton = new System.Windows.Forms.Button();
+            this.cancelAsyncButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // startAsyncButton
+            // 
+            this.startAsyncButton.Location = new System.Drawing.Point(13, 62);
+            this.startAsyncButton.Name = "startAsyncButton";
+            this.startAsyncButton.Size = new System.Drawing.Size(75, 23);
+            this.startAsyncButton.TabIndex = 1;
+            this.startAsyncButton.Text = "Старт";
+            this.startAsyncButton.UseVisualStyleBackColor = true;
+            this.startAsyncButton.Click += new System.EventHandler(this.startAsyncButton_Click);
+            // 
+            // cancelAsyncButton
+            // 
+            this.cancelAsyncButton.Location = new System.Drawing.Point(188, 62);
+            this.cancelAsyncButton.Name = "cancelAsyncButton";
+            this.cancelAsyncButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelAsyncButton.TabIndex = 2;
+            this.cancelAsyncButton.Text = "Отмена";
+            this.cancelAsyncButton.UseVisualStyleBackColor = true;
+            this.cancelAsyncButton.Click += new System.EventHandler(this.cancelAsyncButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 109);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(251, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(13, 161);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(35, 13);
+            this.resultLabel.TabIndex = 4;
+            this.resultLabel.Text = "label1";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 215);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.cancelAsyncButton);
+            this.Controls.Add(this.startAsyncButton);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button startAsyncButton;
+        private System.Windows.Forms.Button cancelAsyncButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ WF.Lessons/Lesson04/WF.Lesson04.Ex18.FibonacciForm/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FibonacciForm
+{
+    public partial class Form1 : Form
+    {
+        private int numberToCompute = 0;
+        private int highestPercentageReached = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeBackgoundWorker();
+        }
+
+        private void InitializeBackgoundWorker()
+        {
+            backgroundWorker1.DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
+            backgroundWorker1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker1_RunWorkerCompleted);
+            backgroundWorker1.ProgressChanged += new ProgressChangedEventHandler(backgroundWorker1_ProgressChanged);
+        }
+
+        private void startAsyncButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = String.Empty;
+            this.numericUpDown1.Enabled = false;
+            this.startAsyncButton.Enabled = false;
+            this.cancelAsyncButton.Enabled = true;
+            
+            numberToCompute = (int)numericUpDown1.Value;
+            
+            highestPercentageReached = 0;
+            
+            // Запуск асинхронной операции
+            backgroundWorker1.RunWorkerAsync(numberToCompute);
+        }
+
+        private void cancelAsyncButton_Click(object sender, EventArgs e)
+        {
+            // Отмена асинхронной операции
+            this.backgroundWorker1.CancelAsync();
+            
+            cancelAsyncButton.Enabled = false;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            
+            // результат арифметической операции
+            e.Result = ComputeFibonacci((int)e.Argument, worker, e);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled) // проверка отмены операции
+            {
+                resultLabel.Text = "Canceled";
+                progressBar1.Value = 0;
+            }
+            else
+            {
+                // успешное завершение операции - получение результата
+                
+                resultLabel.Text = e.Result.ToString();
+            }
+
+            this.numericUpDown1.Enabled = true;
+            startAsyncButton.Enabled = true;
+
+            cancelAsyncButton.Enabled = false;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.progressBar1.Value = e.ProgressPercentage;
+        }
+
+
+
+        // метод выполняется  в другом потоке
+        
+        long ComputeFibonacci(int n, BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            if ((n < 0) || (n > 91))
+            {
+                throw new ArgumentException("value must be >= 0 and <= 91", "n");
+            }
+
+            long result = 0;
+            if (worker.CancellationPending)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                if (n < 2)
+                {
+                    result = 1;
+                }
+                else
+                {
+                    result = ComputeFibonacci(n - 1, worker, e) + ComputeFibonacci(n - 2, worker, e);
+                }
+                // вычисление для отображения хода выполнения процесса
+                int percentComplete = (int)((float)n / (float)numberToCompute * 100);
+                if (percentComplete > highestPercentageReached)
+                {
+                    highestPercentageReached = percentComplete;
+                    worker.ReportProgress(percentComplete);
+                }
+            }
+            return result;
+        }
+
+
+    }
+}
