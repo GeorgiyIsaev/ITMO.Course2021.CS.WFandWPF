55d6f39fa6cd685528c718c49643d78ab899a74d
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-21 19:16:15 +0300

    add less04 ex04

---
+++ WF.Lessons/Lesson04/WF.Lesson04.Ex04.AddWithThreads/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AddWithThreads
+{
+    class AddParams
+    {
+        public int a, b;
+        public AddParams(int numbl, int numb2)
+        {
+            a = numbl; b = numb2; 
+        }
+    }
+
+    class Program
+    {
+        static void Add(object data)
+        {
+            if (data is AddParams)
+            {
+                Console.WriteLine("ID of thread in Add () : {0}",
+                Thread.CurrentThread.ManagedThreadId);
+                AddParams ap = (AddParams)data;
+                Console.WriteLine("{0} + {1} is {2}", ap.a, ap.b, ap.a + ap.b);
+            }
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ID of thread in Main(): {0}",
+Thread.CurrentThread.ManagedThreadId);
+            // Создать объект AddParams для передачи вторичному потоку.
+            AddParams ар = new AddParams(10, 10);
+            Thread t = new Thread(new ParameterizedThreadStart(Add));
+            t.Start(ар);
+            // Подождать, пока другие потоки завершатся.
+            Thread.Sleep(5);
+            Console.ReadLine();
+
+        }
+    }
+}
