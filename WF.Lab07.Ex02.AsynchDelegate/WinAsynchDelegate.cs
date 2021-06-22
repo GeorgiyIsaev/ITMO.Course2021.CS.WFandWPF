18a604d7ac964b62301111210332f1e38359efcf
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-22 23:26:48 +0300

    create lab07 ex02

---
+++ WF.Lab07.Ex02.AsynchDelegate/WinAsynchDelegate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF.Lab07.Ex02.AsynchDelegate
+{
+    public partial class WinAsynchDelegate : Form
+    {
+        public WinAsynchDelegate()
+        {
+            InitializeComponent();
+        }
+    }
+}
