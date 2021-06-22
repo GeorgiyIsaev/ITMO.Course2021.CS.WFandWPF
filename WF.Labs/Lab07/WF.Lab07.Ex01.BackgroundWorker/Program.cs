c8a13b2c3d8ba43d5330e6c78a9004500e2c2ef5
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-22 23:24:24 +0300

    full lab07 ex01

--- WF.Labs/Lab07/WF.Lab07.Ex01.BackgroundWorker/Program.cs
+++ WF.Labs/Lab07/WF.Lab07.Ex01.BackgroundWorker/Program.cs
@@ -16,7 +16,7 @@ namespace WF.Lab07.Ex01.BackgroundWorker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new WinBackgroundWorker());
         }
     }
 }
