09f7053c94c7486b1135f48b3249129becedb99e
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-25 20:33:19 +0300

    add less03ex08

---
+++ WPF.Lessons/Lesson03/WPF.Lesson03.Ex08.WpfRoutedEventBut/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfRoutedEventBut
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Click_1(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            switch (feSource.Name)
+            {
+                case "YesButton":
+                    // do something here ...
+                    MessageBox.Show("YesButton");
+                    break;
+                case "NoButton":
+                    // do something ...
+                    MessageBox.Show("NoButton");
+                    break;
+                case "CancelButton":
+                    // do something ...
+                    MessageBox.Show("CancelButton");
+                    break;
+            }
+            e.Handled = true;
+
+
+        }
+    }
+}
