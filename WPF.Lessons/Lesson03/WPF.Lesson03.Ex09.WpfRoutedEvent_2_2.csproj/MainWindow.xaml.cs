2f12cc1cc43aff793f10ca7c0d31216bcbb60ea4
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-25 20:49:54 +0300

    add less03ex09

---
+++ WPF.Lessons/Lesson03/WPF.Lesson03.Ex09.WpfRoutedEvent_2_2.csproj/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfRoutedEvent_2_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+          //  MessageBox.Show("Event by textbox");
+            e.Handled = (bool)radBut1.IsChecked;
+
+        }
+
+        private void Grid_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+          //  MessageBox.Show("Event by Grid");
+            e.Handled = (bool)radBut2.IsChecked;
+        }
+
+        private void Window_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+          //  MessageBox.Show("Event by Window");
+            e.Handled = (bool)radBut3.IsChecked;
+
+        }
+
+        private void StackPanel_Click_1(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            switch (feSource.Name)
+            {
+                case "butAdd":
+                    // do something here ...
+                    double a = Double.Parse(txtBox.Text);
+                    a += a;
+                    txtBox.Text = String.Format("{0:#.##}", a);
+                  //  MessageBox.Show("YesButton");
+                    break;
+                case "butMult":
+                    // do something ...
+                    double b = Double.Parse(txtBox.Text);
+                    b *= b;
+                    txtBox.Text = String.Format("{0:#.##}", b);
+                //    MessageBox.Show("NoButton");
+                    break;
+
+            }
+            e.Handled = true;
+        }
+    }
+}
