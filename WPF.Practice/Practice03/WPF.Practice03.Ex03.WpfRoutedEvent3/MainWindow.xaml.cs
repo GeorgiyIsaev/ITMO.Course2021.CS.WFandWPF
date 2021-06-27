6c195711d7a0551896a0c2a3b82707618c829485
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-27 16:44:38 +0300

    full prac03 ex03

--- WPF.Practice/Practice03/WPF.Practice03.Ex03.WpfRoutedEvent3/MainWindow.xaml.cs
+++ WPF.Practice/Practice03/WPF.Practice03.Ex03.WpfRoutedEvent3/MainWindow.xaml.cs
@@ -20,9 +20,81 @@ namespace WPF.Practice03.Ex03.WpfRoutedEvent3
     /// </summary>
     public partial class MainWindow : Window
     {
+        string nameFile = "username.txt";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void setText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SetBut.IsEnabled = true;
+            RetBut.IsEnabled = true;
+        }
+
+        bool isDataDirty = false;
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.isDataDirty)
+            {
+                string msg = "Данные были изменены, но не сохранены!\n Закрыть окно без сохранения?";
+                MessageBoxResult result = MessageBox.Show(msg, "Контроль данных", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void New_Win_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            try
+            {            
+            switch (feSource.Name)
+                {
+                    case "SetBut":
+                        SetButClick();
+                        break;
+                    case "RetBut":
+                        RetButClick();
+                        break;
+                }
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void SetButClick()
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(nameFile))
+            {
+                sw.WriteLine(SetText.Text);               
+                RetBut.IsEnabled = true;
+                isDataDirty = false;
+            }
+        }
+        private void RetButClick()
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(nameFile))
+            {
+                sw.WriteLine(RetLabel.Content);               
+                RetBut.IsEnabled = true;
+                isDataDirty = false;
+            }
         }
     }
 }
