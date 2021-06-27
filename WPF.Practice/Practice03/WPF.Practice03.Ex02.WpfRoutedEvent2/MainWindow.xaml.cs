93384914785f200ed013f7e33163a1cc8a227f7c
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-27 16:21:41 +0300

    fix prac03 ex02

--- WPF.Practice/Practice03/WPF.Practice03.Ex02.WpfRoutedEvent2/MainWindow.xaml.cs
+++ WPF.Practice/Practice03/WPF.Practice03.Ex02.WpfRoutedEvent2/MainWindow.xaml.cs
@@ -24,5 +24,24 @@ namespace WPF.Practice03.Ex02.WpfRoutedEvent2
         {
             InitializeComponent();
         }
+        private void StackPanel_Click_1(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            double a = Double.Parse(txtBox.Text);
+            switch (feSource.Name)
+            {
+                case "butAdd":
+                    a += a;
+                    break;
+                case "butMult":
+                    a *= a;
+                    break;
+                case "butSqrt":
+                    a = Math.Sqrt(a);                
+                    break;
+            }
+            e.Handled = true;
+            txtBox.Text = String.Format("{0:#.##}", a);
+        }
     }
 }
