6d11b58dfdeef10e8473e343526695d887582b30
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-27 12:28:49 +0300

    fix prac01 ex01

--- WPF.Practice/Practice01/WPF.Practice01.Ex01.WpfHello/MainWindow.xaml.cs
+++ WPF.Practice/Practice01/WPF.Practice01.Ex01.WpfHello/MainWindow.xaml.cs
@@ -24,6 +24,71 @@ namespace WPF.Practice01.Ex01.WpfHello
         {
             InitializeComponent();
             lbl.Content = "Добрый день!";
+            SetBut.IsEnabled = false;
+            RetBut.IsEnabled = false;
+        }
+
+        private void RetBut_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.StreamReader sr = null;
+            try
+            {
+                using (sr = new System.IO.StreamReader("username.txt"))
+                    retLabel.Content = "Приветствую Вас, уважаемый " + sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                    sr.Close();
+            }
+            isDataDirty = true;
+        }
+
+        private void SetBut_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.StreamWriter sw = null;
+            try
+            {
+                sw = new System.IO.StreamWriter("username.txt");
+                sw.WriteLine(setText.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sw != null)
+                    sw.Close();
+            }
+            isDataDirty = false;
+        }
+
+        private void setText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SetBut.IsEnabled = true;
+            RetBut.IsEnabled = true;
+        }
+
+        bool isDataDirty = false;
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.isDataDirty) { 
+                string msg = "Данные были изменены, но не сохранены!\n Закрыть окно без сохранения?"; 
+                MessageBoxResult result = MessageBox.Show(msg, "Контроль данных", MessageBoxButton.YesNo, MessageBoxImage.Warning); 
+                if (result == MessageBoxResult.No) 
+                {
+                    e.Cancel = true; 
+                } }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
