aeef4904c00332c56ed7b96dd69394bc3e9d24a5
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-28 22:14:12 +0300

    fix prac05 ex01 p3

--- WPF.Practice/Practice05/WPF.Practice05.Ex01.Style/MainWindow.xaml.cs
+++ WPF.Practice/Practice05/WPF.Practice05.Ex01.Style/MainWindow.xaml.cs
@@ -20,9 +20,97 @@ namespace WPF.Practice05.Ex01.Style
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MyWindow myWin { get; set; }
+        string nameFile = "username.txt";
         public MainWindow()
         {
             InitializeComponent();
+            CommandBinding abinding = new CommandBinding();
+            abinding.Command = CustomCommands.Launch;
+            abinding.Executed += new ExecutedRoutedEventHandler(Launch_Handler);
+            abinding.CanExecute += new CanExecuteRoutedEventHandler(LaunchEnabled_Handler);
+            this.CommandBindings.Add(abinding);
+        }
+
+        private void LaunchEnabled_Handler(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (bool)check.IsChecked;
+        }
+        private void Launch_Handler(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (myWin == null)
+                myWin = new MyWindow();
+            myWin.Owner = this;
+            var location = New_Win.PointToScreen(new Point(0, 0));
+            myWin.Left = location.X + New_Win.Width;
+            myWin.Top = location.Y;
+            myWin.Show();
+        }
+
+        private void setText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SetBut.IsEnabled = true;
+            RetBut.IsEnabled = true;
+        }
+
+        bool isDataDirty = false;
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.isDataDirty)
+            {
+                string msg = "Данные были изменены, но не сохранены!\n Закрыть окно без сохранения?";
+                MessageBoxResult result = MessageBox.Show(msg, "Контроль данных", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            try
+            {
+                switch (feSource.Name)
+                {
+                    case "SetBut":
+                        SetButClick();
+                        break;
+                    case "RetBut":
+                        RetButClick();
+                        break;
+                }
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void SetButClick()
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(nameFile))
+            {
+                sw.WriteLine(SetText.Text);
+                RetBut.IsEnabled = true;
+                isDataDirty = false;
+            }
+        }
+        private void RetButClick()
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(nameFile))
+            {
+                sw.WriteLine(RetLabel.Content);
+                RetBut.IsEnabled = true;
+                isDataDirty = false;
+            }
         }
     }
 }
