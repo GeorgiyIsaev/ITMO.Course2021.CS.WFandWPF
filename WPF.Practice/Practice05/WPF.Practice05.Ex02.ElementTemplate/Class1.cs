939f66a55fa3d0a3072773eb851d68ef9d9beded
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-28 22:33:06 +0300

    copy ex01

---
+++ WPF.Practice/Practice05/WPF.Practice05.Ex02.ElementTemplate/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.Practice05.Ex02.ElementTemplate
+{
+    class Class1
+    {
+    }
+}
