332371840990c5c721415747a3f75c937bd992f2
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-01 19:13:00 +0300

    add less06 ex04

---
+++ WPF.Lessons/Lesson06/WPF.Lesson06.Ex04.WpfValidation/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfValidation
+{
+    /// <summary>
+    /// Демонстрация проверки введенных данных в WPF с помощью класса ExceptionValidationRule
+    /// Этот класс определяет введенные данные как некорректные, если в процессе ввода возникает какое-либо исключение, 
+    /// например, исключение преобразования типов
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person objPerson;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            objPerson = new Person();
+            this.DataContext = objPerson;
+        }
+
+        private void TextBox_Error(object sender, ValidationErrorEventArgs e)
+        {
+            MessageBox.Show(e.Error.ErrorContent.ToString(),"Внимание", 
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+    }
+}
---
+++ WPF.Lessons/Lesson06/WPF.Lesson06.Ex04.WpfValidation/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfValidation
+{
+    // Для реализации своей логики валидации для класса модели - модель должна реализовать интерфейс IDataErrorInfo
+    public class Person : IDataErrorInfo
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+
+        // реализация интерфейса
+        public string this[string columnName] // индексатор указывает на конкретное свойство в котором произошла ошибка
+        {
+            get
+            {
+                string error = String.Empty;
+                switch (columnName)
+                {
+                    case "Age":
+                        if ((Age < 0) || (Age > 100))
+                        {
+                            error = "Возраст должен быть больше 0 и меньше 100";
+                        }
+                        break;
+                    case "Name":
+                        //Обработка ошибок для свойства Name
+                        break;
+                    case "Position":
+                        //Обработка ошибок для свойства Position
+                        break;
+                }
+                return error;
+            }
+        }
+        public string Error // Свойство используется для указания общей ошибки 
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+    }
+}
