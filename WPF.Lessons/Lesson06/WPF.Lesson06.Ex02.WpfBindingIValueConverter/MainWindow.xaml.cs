1b60670e6e50e08c874b3043ccb835b20451f122
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-01 18:59:28 +0300

    add less06 ex02

---
+++ WPF.Lessons/Lesson06/WPF.Lesson06.Ex02.WpfBindingIValueConverter/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBindingIValueConverter
+{
+      [ValueConversion(typeof(String), typeof(SolidColorBrush))]
+    public class WordToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+          object parameter, CultureInfo culture)
+        {
+            string boundWord = value as string;
+            SolidColorBrush returnBrush = null;
+
+            switch (boundWord)
+            {
+                case "красный":
+                    returnBrush = new SolidColorBrush(Colors.Red);
+                    break;
+                case "синий":
+                    returnBrush = new SolidColorBrush(Colors.Blue);
+                    break;
+                case "желтый":
+                    returnBrush = new SolidColorBrush(Colors.Yellow);
+                    break;
+                case "зеленый":
+                    returnBrush = new SolidColorBrush(Colors.Green);
+                    break;
+                default:
+                    returnBrush = new SolidColorBrush(Colors.Yellow);
+                    break;
+            }
+            return returnBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+          object parameter, CultureInfo culture)
+        {
+            throw new Exception("Не могу конвертировать обратно");
+        }
+    }
+
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
