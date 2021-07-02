0a4ab493ff1dff5ce3acf9ac5750bbed2dc04458
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-02 18:40:25 +0300

    fix

--- WPF.Practice/Practice06/WPF.Practice06.Ex02.Animation.v2/MainWindow.xaml.cs
+++ WPF.Practice/Practice06/WPF.Practice06.Ex02.Animation.v2/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPF.Practice06.Ex02.Animation.v2
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
