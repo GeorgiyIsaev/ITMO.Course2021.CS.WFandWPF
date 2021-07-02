5b23a397973d7c54ba72ca9fb06e46cf4d05b387
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-02 19:26:40 +0300

    add less07 ex05

---
+++ WPF.Lessons/Lesson07/WPF.Lesson07.Ex05.WpfMVVMdemo01/Model/MathFuncs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMVVMdemo01.Model
+{
+    class MathFuncs
+    {
+     //   public static int GetSumOf(int a, int b) => a + b;
+
+        public static int GetSumOf(int a, int b)
+        {
+           return a + b;
+        }
+    }
+}
