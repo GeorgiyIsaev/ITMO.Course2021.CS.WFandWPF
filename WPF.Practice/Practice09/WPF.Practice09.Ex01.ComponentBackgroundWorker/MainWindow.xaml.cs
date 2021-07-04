80db8da043a1437145926dc5abeb984b0e4ff7fc
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-04 14:13:03 +0300

    full prac09 ex01

--- WPF.Practice/Practice09/WPF.Practice09.Ex01.ComponentBackgroundWorker/MainWindow.xaml.cs
+++ WPF.Practice/Practice09/WPF.Practice09.Ex01.ComponentBackgroundWorker/MainWindow.xaml.cs
@@ -30,15 +30,47 @@ namespace WPF.Practice09.Ex01.ComponentBackgroundWorker
             aWorker.RunWorkerCompleted += aWorker_RunWorkerCompleted;
         }
 
-        private void Grid_Loaded(object sender, RoutedEventArgs e)
-        {   
-        }
+     
         private void aWorker_DoWork(object sender,
             System.ComponentModel.DoWorkEventArgs e)
-        { 
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                for (int j = 1; j <= 10000000; j++)
+                {
+                }
+                if (aWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                UpdateDelegate update = new UpdateDelegate(UpdateLabel);
+                label1.Dispatcher.BeginInvoke(
+                System.Windows.Threading.DispatcherPriority.Normal, update, i);
+            }
         }
         private void aWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
-        { 
+        {
+            if (!(e.Cancelled))
+                label2.Content = "Run Completed";
+            else
+                label2.Content = "Run Cancelled";
+        }
+
+        private delegate void UpdateDelegate(int i);
+        private void UpdateLabel(int i)
+        {
+            label1.Content = "Cycles: " + i.ToString();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            aWorker.RunWorkerAsync();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            aWorker.CancelAsync();
         }
     }
 }
