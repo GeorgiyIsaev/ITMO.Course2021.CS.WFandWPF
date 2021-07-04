a5c2569e05b2f1129d9a9f28a25c7a339dc0dfec
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-04 14:08:17 +0300

    create prac09 ex01

---
+++ WPF.Practice/Practice09/WPF.Practice09.Ex01.ComponentBackgroundWorker/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF.Practice09.Ex01.ComponentBackgroundWorker
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.ComponentModel.BackgroundWorker aWorker = new System.ComponentModel.BackgroundWorker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            aWorker.WorkerSupportsCancellation = true;
+            aWorker.DoWork += aWorker_DoWork;
+            aWorker.RunWorkerCompleted += aWorker_RunWorkerCompleted;
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {   
+        }
+        private void aWorker_DoWork(object sender,
+            System.ComponentModel.DoWorkEventArgs e)
+        { 
+        }
+        private void aWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        { 
+        }
+    }
+}
