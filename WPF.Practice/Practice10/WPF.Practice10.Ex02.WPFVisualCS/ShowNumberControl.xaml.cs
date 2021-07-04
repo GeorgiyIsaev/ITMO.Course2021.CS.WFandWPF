b433b51751142e0b5294a55c5387c02c07e7e026
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-04 14:39:56 +0300

    full prac10 ex02

--- WPF.Practice/Practice10/WPF.Practice10.Ex02.WPFVisualCS/ShowNumberControl.xaml.cs
+++ WPF.Practice/Practice10/WPF.Practice10.Ex02.WPFVisualCS/ShowNumberControl.xaml.cs
@@ -24,16 +24,45 @@ namespace WPF.Practice10.Ex02.WPFVisualCS
         {
             InitializeComponent();
         }
-        private int currNumber = 0;
+
+
         public int CurrentNumber
         {
-            get { return currNumber; }
-            set
-            {
-                currNumber = value;
-                // Передаем в метку текущее значение
-                numberDisplay.Content = CurrentNumber.ToString();
-            }
+            get { return (int)GetValue(CurrentNumberProperty); }
+            set { SetValue(CurrentNumberProperty, value); }
         }
+   
+        public static readonly DependencyProperty CurrentNumberProperty =
+            DependencyProperty.Register("CurrentNumber", typeof(int),
+            typeof(ShowNumberControl), new UIPropertyMetadata(100,
+                   new PropertyChangedCallback(CurrentNumberChanged)),  
+            new ValidateValueCallback(ValidateCurrentNumber));
+
+        public static bool ValidateCurrentNumber(object value)
+        {
+            if (Convert.ToInt32(value) >= 0 && Convert.ToInt32(value) <= 500)
+                return true;
+            else
+                return false;
+        }
+        private static void CurrentNumberChanged(DependencyObject depObj,
+            DependencyPropertyChangedEventArgs args)
+        {
+            ShowNumberControl s = (ShowNumberControl)depObj;
+            Label theLabel = s.numberDisplay;
+            theLabel.Content = args.NewValue.ToString();
+        }
+
+        //private int currNumber = 0;
+        //public int CurrentNumber
+        //{
+        //    get { return currNumber; }
+        //    set
+        //    {
+        //        currNumber = value;
+        //        // Передаем в метку текущее значение
+        //        numberDisplay.Content = CurrentNumber.ToString();
+        //    }
+        //}
     }
 }
