898be49914b14a9c6ba586a4f0e033f08e995b65
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-05 22:56:31 +0300

    create prac12 ex01

---
+++ WPF.Practice/Practice12/WPF.Practice12.Ex01.WorkToProperties/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF.Practice12.Ex01.WorkToProperties
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
