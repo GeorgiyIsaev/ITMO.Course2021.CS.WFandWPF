04d36c83dc0966be5cf3886cfcfb5ff87f2e60b3
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-06 20:03:44 +0300

    full Lab02.ex03

---
+++ WPF.Labs/WPF.Lab02.Ex03.MVVM/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF.Lab02.Ex01.MVVM.ViewModel;
+
+namespace WPF.Lab02.Ex01.MVVM
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var mw = new View.MainWindow 
+            {
+                DataContext = new MainViewModel()
+            };
+            mw.Show();
+        }
+    }
+}
