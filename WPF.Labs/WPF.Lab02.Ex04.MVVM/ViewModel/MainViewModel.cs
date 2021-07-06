7ae237fe4ad7ef5e48b6ceea19c5421ac7431f44
Георгий Исаев <georgiyelbaf@gmail.com>
2021-07-06 20:06:13 +0300

    create wpf.lab02.ex04 (copy ex03)

---
+++ WPF.Labs/WPF.Lab02.Ex04.MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WPF.Lab02.Ex01.MVVM.Model;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF.Lab02.Ex01.MVVM.ViewModel
+{
+    public class MainViewModel
+    {
+        #region Properties
+        /// <summary>
+        /// Get or set people.
+        /// </summary>
+        public PeopleModel People { get; set; }
+        #endregion
+        
+        #region Constructor
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainViewModel()
+        {
+            People = new PeopleModel
+            {
+                FirstName = "First name",
+                LastName = "Last name"
+            };
+            ClickCommand = new Command(arg => ClickMethod());
+        }
+        #endregion
+
+
+        #region Methods
+        /// <summary>
+        /// Click method.
+        /// </summary>
+        private void ClickMethod()
+        {
+            MessageBox.Show("Сlick command");
+        }
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// Get or set ClickCommand.
+        /// </summary>
+        public ICommand ClickCommand { get; set; }
+        #endregion
+
+        
+    }
+}
